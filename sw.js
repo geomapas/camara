afe6d55b6f9f1ac9c1f5bc3393c75cecd7c1afe4
geomapas <sgonzalezgeacam@gmail.com>
2026-07-17 09:29:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'geocam v2.4'; // <-- Recuerda cambiar este número cada vez que subas cambios
+const CACHE_NAME = 'geocam v2.3'; // <-- Recuerda cambiar este número cada vez que subas cambios
 
 const ASSETS = [
   './',
